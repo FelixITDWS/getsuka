5bbfdfb03f44a42ae5f9f7b5c11b6d458189e46f
Felix <twchenfeng@gmail.com>
2025-03-02 22:07:28 +0800

    small refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-import {useState, useEffect, useContext } from 'react';
 import './App.css'
 
-import Navbar from './Components/NavBar';
-
 function App() {
-  useEffect(() => {
-    return (
-      <div ref={navbar} className="fixed z-[100]">
-        <Navbar />
-      </div>
-    )
-  }, []);
+  return(
+    <></>
+  );
 }
 
 export default App
