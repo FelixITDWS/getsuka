88ded513efb1c022343ea3d69a93846fe59cb885
Felix <twchenfeng@gmail.com>
2025-03-10 22:54:57 +0800

    modify profile content

--- src/Pages/Vtuber/Getsuka.tsx
+++ src/Pages/Vtuber/Getsuka.tsx
@@ -18,7 +18,7 @@ function Getsuka() {
           </div>
           <ul className="profile-text">
             <li>
-              <p>是隻世界上最聰明的金魚！</p>
+              <p>是隻世界上比較聰明的金魚！</p>
             </li>
             <li>
               <p>為了尋找同類離開了家鄉，來到了這裡 ✦</p>
@@ -26,12 +26,16 @@ function Getsuka() {
             <li>
               <p>願意成為我的朋友嗎 ( ^ω^)</p>
             </li>
+            <li>
+              <p>喜歡 遊戲 ✶ 拼圖！</p>
+            </li>
             <li className="mail">
               <p>信箱：<a href="mailto:getsuka1008@gmail.com">getsuka1008@gmail.com</a></p>
             </li>
           </ul>
         </div>
         <ul className="profile-tags">
+          <li>個人勢</li>
           <li><span>粉絲名: </span>魚玥飯</li>
           <li><span>FanArt: </span>#華畫</li>
           <li><span>Call: </span>#華到這</li>
