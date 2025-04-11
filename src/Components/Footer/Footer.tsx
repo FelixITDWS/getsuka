1110e2b1e66d9ae4240d3e0565af436a66f97350
Felix <twchenfeng@gmail.com>
2025-04-11 17:20:10 +0800

    fix Github link

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ function Footer() {
         <footer>
             <p>圖片版權為繪師所有，僅用於網站呈現</p>
             <p>
-                Copyright© 2025 Development: <span data-text="Felix"><a href="https://github.com/FelixITDWS" target="_blank">Felix</a></span>
+                Copyright© 2025 Development: <span data-text="Felix"><a href="https://github.com/felixchen-tw" target="_blank">Felix</a></span>
             </p>
         </footer>
     );
